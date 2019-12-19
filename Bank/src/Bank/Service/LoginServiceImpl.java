25b1d803ddb70f3b176bcb6355ff6fac517cae59
young <sey4848@gmail.com>
2019-12-19 19:53:56 +0900

    19.12.19_1953
    [Controller]
    > LoginServiceImpl 메서드 수정
    
    [프론트]
    > Login.fxml 컨트롤러 지정

--- Bank/src/Bank/Service/LoginServiceImpl.java
+++ Bank/src/Bank/Service/LoginServiceImpl.java
@@ -16,38 +16,49 @@ public class LoginServiceImpl implements LoginService{
 
 	@Override
 	public void LoginProc(Parent root) {
-		String id = new String();
-		String pw = new String();
-		if(!GetIdPw(root, id, pw)) return;
+		String id = GetId(root);
+		String pw = GetPw(root);
+		if(id==null || pw==null) return;
 		
-		if(!true) {
-			System.out.println("로그인 실패");
+		DatabaseService dbSQL = new DatabaseServiceImpl();
+		if(!dbSQL.Open()) return;
+		if(!dbSQL.IdEnter(id, pw)) {
 			return;
 		}
 		
-		System.out.println("로그인 성공");
+		common.InfoMsg("로그인 성공");
 	}
-	private boolean GetIdPw(Parent root, String id, String pw) {
+	private String GetId(Parent root) {
 		TextField userId = common.GetTextField(root, fx_Id);
-		TextField userPw = common.GetTextField(root, fx_Id);
-		if(userId==null || userPw==null) {
-			common.ErrorMsg("Login Error", "아이디, 비밀번호를 불러오는데 실패하였습니다.\n 관리자에게 문의해주세요.");
-			return false;
+		if(userId==null) {
+			common.ErrorMsg("Login Error", "아이디를 불러오는데 실패하였습니다.\n 관리자에게 문의해주세요.");
+			return null;
 		}
 		
-		id = userId.getText();
-		pw = userPw.getText();
+		String id = userId.getText();
 		if(id.isEmpty()) {
 			common.InfoMsg("아이디를 입력해 주세요.");
 			userId.requestFocus();
-			return false;
-		}else if(pw.isEmpty()) {
-			common.InfoMsg("비밀번호를 입력해 주세요.");
-			userPw.requestFocus();
-			return false;
+			return null;
 		}
 		
-		return true;
+		return id;
+	}
+	private String GetPw(Parent root) {
+		TextField userPw = common.GetTextField(root, fx_Pw);
+		if(userPw==null) {
+			common.ErrorMsg("Login Error", "비밀번호를 불러오는데 실패하였습니다.\n 관리자에게 문의해주세요.");
+			return null;
+		}
+		
+		String pw = userPw.getText();
+		if(pw.isEmpty()) {
+			common.InfoMsg("아이디를 입력해 주세요.");
+			userPw.requestFocus();
+			return null;
+		}
+		
+		return pw;
 	}
 
 	@Override
